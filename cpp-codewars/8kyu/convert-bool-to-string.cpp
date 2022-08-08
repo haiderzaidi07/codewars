647d58ece20fca8224ac10eaf7aa326ed814aa2c
haiderzaidi07 <smhaider072@gmail.com>
2022-08-08 19:20:35 +0500

    initial commit

---
+++ cpp-codewars/8kyu/convert-bool-to-string.cpp
@@ -0,0 +1,10 @@
+// Description:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Solution:
+
+#include <string>
+
+std::string boolean_to_string(bool b){
+  return b ? "true" : "false";
+}
