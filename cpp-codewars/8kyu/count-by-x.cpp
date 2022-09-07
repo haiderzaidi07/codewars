f291fc853ef60d3a3a333191f9df3dd6b8c0a2f3
haiderzaidi07 <smhaider072@gmail.com>
2022-09-07 21:05:01 +0500

    Returning array with elements which are multiples of x

---
+++ cpp-codewars/8kyu/count-by-x.cpp
@@ -0,0 +1,24 @@
+// Description:
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// countBy(1,10)  should return  {1,2,3,4,5,6,7,8,9,10}
+// countBy(2,5)  should return {2,4,6,8,10}
+
+// Solution:
+
+#include <vector>
+std::vector<int> countBy(int x,int n){
+  
+    std::vector<int> arr;
+
+    for(int i = x; i <= (x*n); i += x){
+        arr.push_back(i);
+    }
+
+    return arr;
+}
